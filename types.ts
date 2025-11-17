15b79deec6b46d2cd3c264c80ba9ac9c7ac7464d
frederickgaragedoors <frederickgaragedoors@gmail.com>
2025-11-17 09:39:56 -0500

    Update types.ts

--- types.ts
+++ types.ts
@@ -20,11 +20,11 @@ export interface DefaultFieldSetting {
 export type JobStatus = 'Scheduled' | 'In Progress' | 'Awaiting Parts' | 'Completed' | 'Invoiced';
 
 export const jobStatusColors: Record<JobStatus, { base: string, text: string }> = {
-  Scheduled: { base: 'bg-sky-100', text: 'text-sky-800' },
-  'In Progress': { base: 'bg-yellow-100', text: 'text-yellow-800' },
-  'Awaiting Parts': { base: 'bg-purple-100', text: 'text-purple-800' },
-  Completed: { base: 'bg-green-100', text: 'text-green-800' },
-  Invoiced: { base: 'bg-slate-200', text: 'text-slate-700' },
+  Scheduled: { base: 'bg-sky-100 dark:bg-sky-900', text: 'text-sky-800 dark:text-sky-200' },
+  'In Progress': { base: 'bg-yellow-100 dark:bg-yellow-900', text: 'text-yellow-800 dark:text-yellow-200' },
+  'Awaiting Parts': { base: 'bg-purple-100 dark:bg-purple-900', text: 'text-purple-800 dark:text-purple-200' },
+  Completed: { base: 'bg-green-100 dark:bg-green-900', text: 'text-green-800 dark:text-green-200' },
+  Invoiced: { base: 'bg-slate-200 dark:bg-slate-700', text: 'text-slate-700 dark:text-slate-200' },
 };
 
 export interface Part {
