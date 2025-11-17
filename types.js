3cd8fefa3c1d2d8f095eb632fb1ced72e166598e
frederickgaragedoors <frederickgaragedoors@gmail.com>
2025-11-17 11:47:03 -0500

    Update types.js

--- types.js
+++ types.js
@@ -3,5 +3,5 @@ export const jobStatusColors = {
   'In Progress': { base: 'bg-yellow-100 dark:bg-yellow-900', text: 'text-yellow-800 dark:text-yellow-200' },
   'Awaiting Parts': { base: 'bg-purple-100 dark:bg-purple-900', text: 'text-purple-800 dark:text-purple-200' },
   Completed: { base: 'bg-green-100 dark:bg-green-900', text: 'text-green-800 dark:text-green-200' },
-  Invoiced: { base: 'bg-slate-200 dark:bg-slate-700', text: 'text-slate-700 dark:text-slate-200' },
+  Paid: { base: 'bg-slate-200 dark:bg-slate-700', text: 'text-slate-700 dark:text-slate-200' },
 };
