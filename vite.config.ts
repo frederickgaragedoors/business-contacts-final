5d70e29e26d61937dd55adcc2266a05aa0d47dee
frederickgaragedoors <frederickgaragedoors@gmail.com>
2025-11-20 18:23:21 -0500

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,16 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // Base must be relative for Electron to load assets from file:// protocol
+      base: './', 
       server: {
         port: 3000,
         host: '0.0.0.0',
+      },
+      build: {
+        chunkSizeWarningLimit: 2000,
+        outDir: 'dist',
+        emptyOutDir: true,
       },
       plugins: [react()],
       define: {
@@ -16,7 +23,7 @@ export default defineConfig(({ mode }) => {
       },
       resolve: {
         alias: {
-          '@': path.resolve(process.cwd(), '.'),
+          '@': path.resolve('.'),
         }
       }
     };
